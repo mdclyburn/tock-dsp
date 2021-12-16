6ed956d02b75e8a3728a18a0dab36a7d220dab98
Marshall Clyburn <mardev2013@gmail.com>
2021-12-15 21:50:33 -0500

    Phase two of interrupt handling
    Provide an interrupt handler to ignore and disable an interrupt (in
    the case of the UART0 interrupt which I want core0 to handle). This
    is the alternative to modifying the UART code to not enable the
    interrupt if the executing core is core1 (doesn't feel like the
    right solution).
    
    Copy the IPSR into r0 whenever entering the interrupt handlers so
    it is easier to see what interrupt got control flow here.

--- boards/raspberry_pi_pico/src/aspk/interrupt.rs
+++ boards/raspberry_pi_pico/src/aspk/interrupt.rs
@@ -24,20 +24,74 @@ pub unsafe fn configure() {
         };
     };
 
-    for irq in &mut IRQS {
-        *irq = ignored_interrupt as usize;
+    for (no, handler) in (0..).zip(&mut IRQS) {
+        *handler = match no {
+            // UART0, we let core0 handle servicing this.
+            // However, if core1 sends something through the UART,
+            // the HAL enables the interrupt, so we ignore it and disable it.
+            20 => ignore_disable_interrupt_handler as usize,
+            _ => fail_interrupt as usize,
+        }
     }
 }
 
 /// "Handler" for interrupts that are not being explicitly handled by ASPK.
 #[allow(dead_code)]
+#[no_mangle]
 pub unsafe extern "C" fn ignored_interrupt() {
+    asm!(
+        "mrs r0, ipsr",
+    );
+
     loop { cortexm0p::support::wfe(); }
 }
 
 /// "Handler" for interrupts that should be handled;
 /// ignoring them is dangerous.
 #[allow(dead_code)]
+#[no_mangle]
 unsafe fn fail_interrupt() {
+    asm!(
+        "mrs r0, ipsr"
+    );
+
     loop { cortexm0p::support::wfe(); }
+}
+
+#[allow(dead_code)]
+#[naked]
+#[no_mangle]
+unsafe extern "C" fn ignore_disable_interrupt_handler() {
+    asm!(
+        "cpsid i",
+
+        // Find out which interrupt number this is.
+        "mrs r0, ipsr",
+        "movs r1, #0b11111",
+        "ands r0, r1",
+        "subs r0, #16",
+        // r0 = interrupt number
+
+        // Clear the interrupt.
+        // Get the mask for the interrupt.
+        "movs r1, #1",
+        "lsls r1, r0",
+        // r1 = mask for the interrupt number
+        // Write to ICPR to clear it.
+        "ldr r2, ICPR",
+        "str r1, [r2]",
+
+        // Disable the interrupt.
+        "ldr r2, ICER",
+        "str r1, [r2]",
+
+        // Return to whatever the system was doing prior to the interrupt.
+        "bx lr",
+
+        ".align 4",
+        "ICPR: .word 0xe000e200",
+        "ICER: .word 0xe000e180",
+
+        options(noreturn)
+    )
 }
